1191042a01b39fcb491035aa921b34cb143c1913
yurizinho28 <yurisantosbrito2010@gmail.com>
2026-09-19 12:15:16 -0300

    adicionar página de previsão do tempo, remover script desnecessário e iniciar estilização da última seção

--- interface/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu");
-const iconeMenu = document.getElementById("iconeMenu");
-
-iconeMenu.addEventListener("click", ()=> {
-    menu.dataset.ativo = menu.dataset.ativo === "true" ? "false" : "true";
-})
