f51f8a27f9ae3d5f333f1e9818b2c3717fefd45f
yurizinho28 <yurisantosbrito2010@gmail.com>
2026-09-24 19:08:32 -0300

    feat: Add main website structure and styling for Drone For All
    - Created index.html with header, main sections, and footer.
    - Added navigation links and icons for weather and scanning features.
    - Implemented sections for project description, objectives, and team information.
    - Developed monitoring section with dynamic content placeholders.
    - Introduced script.js for handling user interactions and terms acceptance.
    - Added CSS styles for layout, responsiveness, and visual effects.
    
    feat: Create clima.html for weather forecast page
    
    - Established clima.html with basic structure and metadata.
    - Linked to clima.css for styling.
    
    feat: Add license.html for terms of use
    
    - Developed license.html outlining usage terms and restrictions.
    - Linked to license.css for styling.
    
    style: Add CSS files for clima and license pages
    
    - Created clima.css for weather page styling.
    - Created license.css for terms of use page styling.
    
    style: Implement main CSS styles for the website
    
    - Defined global styles and variables in style.css.
    - Styled header, main content, sections, and footer for consistency.

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const btnAceitar = document.getElementById("accept");
 const btnRecusar = document.getElementById("decline");
 const climaBtn = document.getElementById("clima");
 
+window.addEventListener("DOMContentLoaded", () => {
+    function isNewWindow() {
+        sessionStorage.getItem("newWindow") === "false" ? termos.dataset.ativo = "false" : termos.dataset.ativo = "true";
+    }
+
+    isNewWindow();
+        
+});
+
 btnAceitar.addEventListener("click", () => {
     termos.dataset.ativo = "false";
+    sessionStorage.setItem("newWindow", "false");
 });
 
 btnRecusar.addEventListener("click", () => {
